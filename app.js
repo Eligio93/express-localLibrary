ca7174db9101332d2543cd2e5c50553db087ad3d
Eligio93 <eligio.cristantielli@gmail.com>
2024-04-25 11:44:45 +1000

    import compression

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const catalogRouter = require('./routes/catalog')
+const compression = require('compression');
 
 const app = express();
 const mongoose=require('mongoose');
@@ -16,7 +17,7 @@ main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
 }
-
+app.use(compression())
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/catalog', catalogRouter)
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
